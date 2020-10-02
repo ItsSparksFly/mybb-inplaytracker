bb4a36130333cffba058049e56f2773bc0cb3b84
Julia Roloff <julia.roloff@qbeyond.de>
2020-10-02 22:58:10 +0200

    initial commit, unfinished project

---
+++ inc/languages/deutsch_du/inplaytracker.lang.php
@@ -0,0 +1,26 @@
+<?php
+$l['ipt'] = "Inplaytracker";
+$l['ipt_newthread_options'] = "Inplaytracker-Optionen";
+$l['ipt_newthread_partners'] = "Mitspieler";
+$l['ipt_newthread_partners_description'] = "Gib deine Mitspieler in der Reihenfolge an, in der gepostet werden soll.";
+$l['ipt_newthread_date'] = "Datum";
+$l['ipt_newthread_location'] = "Ort";
+$l['ipt_newthread_private'] = "Öffentliche Szene";
+$l['ipt_newthread_private_description'] = "Findet diese Szene in der Öffentlichkeit statt?";
+$l['ipt_newthread_private_open'] = "öffentlich";
+$l['ipt_newthread_private_closed'] = "privat";
+$l['ipt_newthread_description'] = "Szenenbeschreibung";
+$l['ipt_newthread_description_description'] = "Beschreibe in maximal 140 Zeichen, worum es in dieser Szene geht.";
+$l['ipt_forumdisplay_characters'] = "Darsteller";
+$l['ipt_forumdisplay_date'] = "Datum";
+$l['ipt_postbit_by'] = "mit";
+$l['ipt_header_tracker'] = "Szenen";
+$l['ipt_header_tracker_open'] = "davon offen";
+
+// Alerts
+$l['inplaytracker_newthread'] = "{1} hat dir eine neue Szene erstellt!";
+$l['inplaytracker_newreply'] = "{1} hat auf eure Szene \"{2}\" geantwortet!";
+$l['myalerts_setting_inplaytracker_newthread'] = "Benachrichtigung bei neu erstellten Szenen bekommen?";
+$l['myalerts_setting_inplaytracker_newreply'] = "Benachrichtigung bei neu erstellten Antworten auf Szenen bekommen?";
+
+?>
